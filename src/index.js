c42ae15d1dca77e5d96a5d1149c955c30b40186f
joshpson <jpearson846@gmail.com>
2018-06-25 14:47:19 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>Components Need To Be Mounted</div>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
